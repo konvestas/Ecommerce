7d6b98fbae20e5146dbdd4bff9b605cf16fc5860
Onur Akgulay <126293809+konvestas@users.noreply.github.com>
2025-02-08 16:36:28 +0300

    config duzeltildi

--- Ecommerce/src/main/java/com/proje/Ecommerce/config/SecurityConfig.java
+++ Ecommerce/src/main/java/com/proje/Ecommerce/config/SecurityConfig.java
@@ -1,15 +1,33 @@
-package com.proje.Ecommerce.config;
+package com.proje.Ecommerce.config;//package com.proje.Ecommerce.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//@Configuration
+//public class SecurityConfig {
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // CSRF'yi devre dışı bırak
+                .authorizeHttpRequests()
+                .anyRequest().permitAll(); // Tüm uç noktaları herkese açık yap
+        return http.build();
     }
 }

dc6e73a7413c1298ee016d5c4251df8bd894b485
Onur Akgulay <126293809+konvestas@users.noreply.github.com>
2025-02-08 16:13:01 +0300

    Get set metodlari duzeltildi

--- Ecommerce/src/main/java/com/proje/Ecommerce/dto/ProductDTO.java
+++ Ecommerce/src/main/java/com/proje/Ecommerce/dto/ProductDTO.java
@@ -4,13 +4,13 @@ import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-
-@Data
+@Getter
+@Setter
 public class ProductDTO {
 
     @NotBlank(message = "Product name is required")

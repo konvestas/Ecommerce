54cf7ea6a0158edf1890f50eea77e4215c9da3d6
Onur Akgulay <126293809+konvestas@users.noreply.github.com>
2025-02-08 16:39:43 +0300

    gecici olarak yoruma alindi

--- Ecommerce/src/main/java/com/proje/Ecommerce/controller/UserController.java
+++ Ecommerce/src/main/java/com/proje/Ecommerce/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.proje.Ecommerce.controller;
 
+import com.proje.Ecommerce.dto.Response;
 import com.proje.Ecommerce.dto.UserDTO;
+import com.proje.Ecommerce.model.User;
+import com.proje.Ecommerce.service.IUserService;
 import com.proje.Ecommerce.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,16 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+//    @Autowired
+//    private IUserService iUserService;
+//
+//    @PostMapping("/register")
+//    public ResponseEntity<String> registerUser(@RequestBody UserDTO userfromWeb) {
+//        Response response = iUserService.register(userfromWeb);
+//        return ResponseEntity.ok("User registered successfully!");
+//    }
+//
+//    @PostMapping("/login")
+//    public ResponseEntity<String> loginUser(@RequestBody UserDTO userfromWeb) {
+//        Response response = iUserService.login(userfromWeb);
+//        return ResponseEntity.ok("User login successfully!");
+//    }
 
-    @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@RequestBody UserDTO userDTO) {
-        userService.registerUser(userDTO);
-        return ResponseEntity.ok("User registered successfully!");
-    }
 }
 

e92d01e8b6121ad4ebbc8d11f408c8115db93584
Onur Akgulay <126293809+konvestas@users.noreply.github.com>
2025-02-08 16:38:21 +0300

    service gelistirildi

--- Ecommerce/src/main/java/com/proje/Ecommerce/service/UserService.java
+++ Ecommerce/src/main/java/com/proje/Ecommerce/service/UserService.java
@@ -1,32 +1,68 @@
 package com.proje.Ecommerce.service;
 
+import com.proje.Ecommerce.dto.LoginRequest;
+import com.proje.Ecommerce.dto.Response;
 import com.proje.Ecommerce.dto.UserDTO;
-import com.proje.Ecommerce.enums.Role;
 import com.proje.Ecommerce.model.User;
 import com.proje.Ecommerce.repository.UserRepository;
+import com.proje.Ecommerce.utils.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Service
-public class UserService {
+public class UserService implements IUserService {
+    @Autowired
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {this.userRepository = userRepository;}
 
 
-    public User registerUser( UserDTO userDTO ){
-        User user = new User();
-        user.setUsername(userDTO.getUsername());
-        user.setPassword(userDTO.getPassword());
-        user.setEmail(userDTO.getEmail());
+    @Override
+    public Response register(@RequestBody UserDTO userInfo){
+        Response response = new Response();
+        try {
 
-        user.setFirstName(userDTO.getFirstName());
-        user.setLastName(userDTO.getLastName());
-        user.setPhoneNumber(userDTO.getPhoneNumber());
+            if (userRepository.existsByEmail(userInfo.getEmail())) {
+                response.setStatusCode(400);
+                response.setMessage("Email " + userInfo.getEmail() + " already exists.");
+                return response;
+            }
+            if (userRepository.existsByPhoneNumber(userInfo.getPhoneNumber())) {
+                response.setStatusCode(400);
+                response.setMessage("Phone number " + userInfo.getPhoneNumber() + " already exists.");
+                return response;
+            }
 
-        user.setRole(Role.USER);
+            User transferedUSer = Utils.userDTOToUser(userInfo);
+            userRepository.save(transferedUSer);
 
-        userRepository.save(user);
-        return user;
+            response.setUser(userInfo);
+            response.setStatusCode(201);
+            response.setMessage("User registered successfully");
+
+        } catch (Exception e){
+            response.setStatusCode(500);
+            response.setMessage("Error during user registration: " + e.getMessage());
+        }
+    return response;
+    }
+
+    @Override
+    public Response login(@RequestBody LoginRequest loginRequest) {
+        Response response = new Response();
+
+//        userRepository.findByUsername(userInfo.getEmail())
+//                 .filter(u -> u.getPassword().equals(userInfo.getPassword())); // Şifre kontrolü
+        return response;
+    }
+
+    @Override
+    public Response authenticate(UserDTO userInfo) {
+        Response response = new Response();
+
+        return response;
     }
 
 }

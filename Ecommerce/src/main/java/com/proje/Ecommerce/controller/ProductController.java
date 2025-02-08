c5b39105cd8b22647f86ebeb839ef05406c6d441
Onur Akgulay <126293809+konvestas@users.noreply.github.com>
2025-02-08 16:41:40 +0300

    gecici olarak yoruma alindi

--- Ecommerce/src/main/java/com/proje/Ecommerce/controller/ProductController.java
+++ Ecommerce/src/main/java/com/proje/Ecommerce/controller/ProductController.java
@@ -1,4 +1,11 @@
 package com.proje.Ecommerce.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+
 }
--- Ecommerce/src/main/java/com/proje/Ecommerce/service/ProductService.java
+++ Ecommerce/src/main/java/com/proje/Ecommerce/service/ProductService.java
@@ -1,4 +1,23 @@
 package com.proje.Ecommerce.service;
 
-public class ProductService {
+import com.proje.Ecommerce.dto.ProductDTO;
+import com.proje.Ecommerce.dto.Response;
+import com.proje.Ecommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class ProductService implements IProductService {
+    @Autowired
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {this.productRepository = productRepository;}
+
+    @Override
+    public Response addProduct(@RequestBody ProductDTO product) {
+        Response response = new Response();
+        return response;
+    }
+
 }
